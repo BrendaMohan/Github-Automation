a6ee81999e37350781469654a292e7c54c85695b
BrendaMohan <110720653+BrendaMohan@users.noreply.github.com>
2023-07-31 21:32:00 +0530

    Update repo_private.sh

--- repo_private.sh
+++ repo_private.sh
@@ -5,10 +5,28 @@ GITHUB_USERNAME="BrendaMohan"
 GITHUB_ACCESS_TOKEN="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"
 REPO_NAME="private-repo"
 
+# Function to check if the repository exists
+check_repo_existence() {
+    repo_url="https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME"
+    response=$(curl -s -H "Authorization: token $GITHUB_ACCESS_TOKEN" "$repo_url")
+    if [[ "$response" == *"Not Found"* ]]; then
+        return 1  # Repository does not exist
+    else
+        return 0  # Repository exists
+    fi
+}
+
 # Function to create a private repository
 create_github_repository() {
     curl -X POST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/user/repos" -d "{\"name\":\"$REPO_NAME\", \"private\": true}"
 }
 
 # Main script
-create_github_repository
+check_repo_existence
+if [ $? -eq 1 ]; then
+    echo "Repository does not exist. Creating the repository..."
+    create_github_repository
+    echo "Repository created successfully!"
+else
+    echo "Repository already exists. Skipping repository creation."
+fi
