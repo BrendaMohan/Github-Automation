2c6c70e2fa297bc2d2bb4900b3a0b9e1ed9f57f7
BrendaMohan <110720653+BrendaMohan@users.noreply.github.com>
2023-08-03 15:04:22 +0530

    Update pull_request.sh

--- pull_request.sh
+++ pull_request.sh
@@ -8,3 +8,12 @@ FEATURE_BRANCH="feature"
 
 # Create a pull request
 curl -X POST -H "Authorization: token $GITHUB_ACCESS_TOKEN" -d "{\"title\":\"Feature Update\", \"body\":\"Please review this feature update.\", \"head\":\"$GITHUB_USERNAME:$FEATURE_BRANCH\", \"base\":\"main\"}" "https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME/pulls"
+
+# Request reviewers for a pull request
+function add_reviewers() {
+  local pr_number=$1
+  local reviewers_json=$(jq -n --argjson reviewers '["'$(IFS=, ; echo "${REVIEWERS[*]}")'"]' \
+    '{reviewers: $reviewers}')
+  curl -X POST -H "Authorization: token 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" -H "Content-Type: application/json" \
+    -d "$reviewers_json" "https://api.github.com/repos/$GITHUB_USERNAME/$REPO_NAME/pulls/$pr_number/requested_reviewers"
+}
